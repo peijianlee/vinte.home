d5b3959526643b6021fd23687df7efaff09e4b5a
培坚 <peijian.li@cx-m.com>
2017-08-12 16:22:57 +0800

    change logo

--- config/config.js
+++ config/config.js
@@ -1,6 +1,12 @@
 var config = {
 	dburl: "mongodb://vinte:LPJ5548744948jd@localhost:27017/lpj_db",
-	port: "3000"
+	port: "3000",
+	global_title_cn: "梵特家具网",
+	global_title_en: "vintehome",
+	global_keyword_cn: "梵特家具网,中山梵特家具网,乡村风格家具,中山乡村风格家具,仿古家具,中山仿古家具",
+	global_keyword_en: "vintehome,country style furniture,antique furniture",
+	global_description_cn: "梵特家具是一家专业的家具制造厂商，其制造范围有批量家具生产、家具定制、家具重设计，家具风格主要是以乡村风格、工业风格、宫廷风格及其他手工艺绘制风格为主，多年销往欧美及其周边多个国家。",
+	global_description_en: "vintehome is a professional furniture manufacturer, its manufacturing range of mass production of furniture, furniture custom, furniture re-design, furniture style is mainly rural style, industrial style, court style and other hand-painted style, Europe and the United States and its surrounding countries."
 }
 
 module.exports = config
